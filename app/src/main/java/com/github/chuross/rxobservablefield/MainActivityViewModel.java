83462e13fe56556861f20081153e2ef36d41f97e
chuross <azarasi.mo.mo@gmail.com>
2017-08-17 03:49:00 +0900

    update sample code

--- app/src/main/java/com/github/chuross/rxobservablefield/MainActivityViewModel.java
+++ app/src/main/java/com/github/chuross/rxobservablefield/MainActivityViewModel.java
@@ -11,17 +11,32 @@ public class MainActivityViewModel {
 
     public ObservableField<String> textField = new ObservableField<>("");
     public ReadOnlyRxObservableField<Integer> lengthField = getStringLengthObservableField(textField);
+    public ReadOnlyRxObservableField<String> upperStringTextField = getUpperStringTextObservableField(textField);
 
     private ReadOnlyRxObservableField<Integer> getStringLengthObservableField(ObservableField<String> field) {
         Observable<String> source = ObservableUtils.toObservable(field);
 
-        Observable<Integer> lengthSource = source.map(new Function<String, Integer>() {
+        Observable<Integer> modifiedSource = source.map(new Function<String, Integer>() {
             @Override
             public Integer apply(@NonNull String text) throws Exception {
                 return text.length();
             }
         });
 
-        return new ReadOnlyRxObservableField<>(lengthSource);
+        return new ReadOnlyRxObservableField<>(modifiedSource);
     }
+
+    private ReadOnlyRxObservableField<String> getUpperStringTextObservableField(ObservableField<String> field) {
+        Observable<String> source = ObservableUtils.toObservable(field);
+
+        Observable<String> modifiedSource = source.map(new Function<String, String>() {
+            @Override
+            public String apply(@NonNull String text) throws Exception {
+                return text.toUpperCase();
+            }
+        });
+
+        return new ReadOnlyRxObservableField<>(modifiedSource);
+    }
+
 }
