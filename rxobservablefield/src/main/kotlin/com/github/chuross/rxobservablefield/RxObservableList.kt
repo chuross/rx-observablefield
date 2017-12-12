bc2b8402d47e5309f07e0491403c8934d528b119
chuross <azarasi.mo.mo@gmail.com>
2017-12-13 03:05:41 +0900

    fix rx stream

--- rxobservablefield/src/main/kotlin/com/github/chuross/rxobservablefield/RxObservableList.kt
+++ rxobservablefield/src/main/kotlin/com/github/chuross/rxobservablefield/RxObservableList.kt
@@ -3,11 +3,26 @@ package com.github.chuross.rxobservablefield
 import android.databinding.ObservableArrayList
 import com.github.chuross.rxobservablefield.internal.ObservableUtils
 import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
 
 class RxObservableList<T> : ObservableArrayList<T>() {
 
-    @Transient
-    private var observable: Observable<List<T>>? = null
-    val rx: Observable<List<T>> get() = observable ?: ObservableUtils.toObservable(this).also { observable = it }
+    @Transient private var observable: Observable<List<T>>? = null
+    @Transient private var emptyEvent: PublishSubject<List<T>>? = null
+    val rx: Observable<List<T>> get() = observable ?: Observable.merge(listOf(
+            ObservableUtils.toObservable(this).also { observable = it },
+            PublishSubject.create<List<T>>().also { emptyEvent = it }
+    ))
 
+    override fun addAll(elements: Collection<T>): Boolean {
+        return super.addAll(elements).also {
+            if (isEmpty()) emptyEvent?.onNext(toList())
+        }
+    }
+
+    override fun addAll(index: Int, elements: Collection<T>): Boolean {
+        return super.addAll(index, elements).also {
+            if (isEmpty()) emptyEvent?.onNext(toList())
+        }
+    }
 }
